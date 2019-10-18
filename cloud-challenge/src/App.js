2f17bc0134aef91a5322b0600cea3d338f7f2856
Ngari Ndung'u <ngari.ndungu@gmail.com>
2019-10-18 07:35:31 +0300

    create react app for some reason

---
+++ cloud-challenge/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Nothing to see here. ALC4 <strong>Banzai!!</strong>
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a> ...or dont?
+      </header>
+    </div>
+  );
+}
+
+export default App;
